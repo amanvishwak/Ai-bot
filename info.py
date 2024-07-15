6aeada2c8450c12d1f7947353d4818d39a0d1e3c
amanvishwak <147085734+amanvishwak@users.noreply.github.com>
2024-07-15 06:11:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,17 +1,16 @@
-# ©️biisal jai shree krishna 😎
 from os import environ
 
-API_ID = environ.get("API_ID", "")
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-BOT_NAME = environ.get("BOT_NAME", "BISAL CHAT BOT")
-ADMIN = int(environ.get("ADMIN", ""))
-CHAT_GROUP = int(environ.get("CHAT_GROUP", "-1001812797837"))
-ADMIN_NAME = environ.get("ADMIN_NAME", "Bisal")
-LOG_CHANNEL = environ.get("LOG_CHANNEL", "")
-MONGO_URL = environ.get("MONGO_URL", "")
+API_ID = environ.get("API_ID", "12380656")
+API_HASH = environ.get("API_HASH", "d927c13beaaf5110f25c505b7c071273")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7328112987:AAE-m8fhi09-5GtdiSI0Lii8Qvpkg7Snnfs")
+BOT_NAME = environ.get("BOT_NAME", "AV_GPT_BOT")
+ADMIN = int(environ.get("ADMIN", "5977931010"))
+CHAT_GROUP = int(environ.get("CHAT_GROUP", "-1002028053413"))
+ADMIN_NAME = environ.get("ADMIN_NAME", "BOT_OWNER26")
+LOG_CHANNEL = environ.get("LOG_CHANNEL", "-1002110971750")
+MONGO_URL = environ.get("MONGO_URL", "mongodb+srv://aman991932:aman@cluster0.qp6vfyg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 AUTH_CHANNEL = int(
-    environ.get("AUTH_CHANNEL", "-100XXXXXXXXXX")
+    environ.get("AUTH_CHANNEL", "-1002012150170")
 )  # add your channel id for force subscribe
 FSUB = environ.get("FSUB", True)
 STICKERS_IDS = (
